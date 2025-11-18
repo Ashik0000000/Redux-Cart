9e721ebdb2cacc84fb44ff07deb0b9571db65dd2
Ashik0000000 <muhammedashik85928@gmail.com>
2025-11-18 17:03:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Wishlist from './pages/Wishlist'
+import Cart from './pages/Cart'
+import View from './pages/View'
+import Pnf from './pages/Pnf'
+
+function App() {
+
+  return (
+<>
+   <Routes>
+     <Route path='/' element={<Home/>}></Route>
+      <Route path='/wishlist' element={<Wishlist/>}></Route>
+       <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/products/:id/view' element={<View/>}></Route>
+         <Route path='/*' element={<Pnf/>}></Route>
+   </Routes>
+ <Footer/>
+ </>
+  )
+}
+
+export default App
